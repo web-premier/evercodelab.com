cbcae504db077da699a450963c40b1c41de4842e
Sergey Lunev <sergey@lunyov.ru>
2012-05-18 13:57:13 +0400

    Update #2: Change translator in config.yml for good looking FOSU pages Added DataFixtures for admin account

---
+++ src/App/UserBundle/DataFixtures/ORM/LoadAdminData.php
@@ -0,0 +1,23 @@
+<?php 
+// src/App/UserBundle/DataFixtures/ORM/LoadAdminData.php
+namespace App\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use App\UserBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPlainPassword('test');
+        $userAdmin->setEnabled(1);
+        $userAdmin->setEmail('test@test.local');
+        $userAdmin->setRoles(array('ROLE_ADMIN'));
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
