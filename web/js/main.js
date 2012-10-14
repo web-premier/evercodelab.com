e834347f7cc576b17e07a89375b5d08740265d75
Roma Lapin <roman.lapin@gmail.com>
2012-10-14 12:48:43 +0400

    js paginate

--- web/js/main.js
+++ web/js/main.js
@@ -44,10 +44,101 @@
 			});
 		});
 	}
-		  
+
+    function paginate(options) {
+        var defaults = {
+            items_per_page : 3
+        }
+        var options = $.extend(defaults,options);
+        var current_page = 0;
+
+        var $item_container = $('.b-portfolio-text ul');
+        var $items = $item_container.children();
+        var $control_container = $('.b-slider');
+
+        // Get the total number of items
+        var total_items = $items.size();
+
+        // Calculate the number of pages needed
+        var number_of_pages = Math.ceil(total_items/options.items_per_page);
+
+        // hide all pages
+        $items.hide();
+        // Show the first page
+        $items.slice(0, options.items_per_page).show();
+
+        // Event handler for 'Prev' link
+        $control_container.find('.prev').click(function(e){
+            e.preventDefault();
+            showPrevPage($(this));
+        });
+
+        // Event handler for 'Next' link
+        $control_container.find('.next').click(function(e){
+            e.preventDefault();
+            showNextPage($(this));
+        });
+
+        function showPrevPage(e){
+            new_page = current_page - 1;
+
+            // Check that we aren't on a boundary link
+            if($(e).hasClass('disabled')){
+                return;
+            }else{
+                gotopage(new_page);
+            }
+        };
+
+        function showNextPage(e){
+            new_page = current_page + 1;
+
+            // Check that we aren't on a boundary link
+            if($(e).hasClass('disabled')){
+                return;
+            }else{
+                gotopage(new_page);
+            }
+        };
+
+        function gotopage(page_num){
+            var ipp = options.items_per_page;
+
+            var isLastPage = false;
+
+            // Find the start of the next slice
+            start_from = page_num * ipp;
+
+            // Find the end of the next slice
+            end_on = start_from + ipp;
+            // Hide the current page
+            var items = $items.hide().slice(start_from, end_on);
+
+            items.show();
+
+            // Set the current page meta data
+            current_page = page_num;
+
+            // Add a class to the next or prev links if there are no more pages next or previous to the active page
+            if (page_num == 0) {
+                $control_container.find('.prev').addClass('disabled');
+                $control_container.find('.next').removeClass('disabled');
+            } else if (page_num == number_of_pages - 1) {
+                $control_container.find('.prev').removeClass('disabled');
+                $control_container.find('.next').addClass('disabled');
+            } else {
+                $control_container.find('.prev').removeClass('disabled');
+                $control_container.find('.next').removeClass('disabled');
+            }
+        };
+
+
+    }
+
 	function init(){
 //		switcher();
 		scrollLine();
+        paginate();
 	}
 	
 	$(function(){
