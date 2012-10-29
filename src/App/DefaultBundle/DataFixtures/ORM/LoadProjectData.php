ee5f68468d8ab19f8493af586a0d95012966a1bd
Roma Lapin <roman.lapin@gmail.com>
2012-10-29 18:02:57 +0400

    portfolio translation

--- src/App/DefaultBundle/DataFixtures/ORM/LoadProjectData.php
+++ src/App/DefaultBundle/DataFixtures/ORM/LoadProjectData.php
@@ -10,15 +10,6 @@ use App\DefaultBundle\Entity\Portfolio;
 class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
 {
 
-
-//Внутренние и Open Source проекты:
-//http://php-guide.evercodelab.com/
-//http://thebookofknowledge.evercodelab.com/
-//Бандлы для Symfony2:
-//https://github.com/EvercodeLab/FaqBundle
-//https://github.com/EvercodeLab/BannerBundle
-//https://github.com/EvercodeLab/EvercodePageBundle
-
     protected $projects = array(
         array(
             'name' => 'ITOP',
@@ -49,8 +40,8 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'Manaflask',
                 'description' => '
-                        Manaflask – популярный игровой портал, предоставляющий материалы от топовых мировых игроков на английском и немецком языках.
-                        Мы поддерживали старую версию сайта, написанную на PHP. В данный момент при нашем участии создается новая версия сайта на Ruby on Rails.
+                        Manaflask is an Internet Gaming Portal dedicated to bringing premium content presented in both English and German created by top teams and individuals.
+                        We were involved in support of current version of site written in PHP. At the moment we are two steps away from launching new version of portal on Ruby on Rails.
                         ',
             ),
         ),
@@ -64,8 +55,8 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'Noob-Club',
                 'description' => '
-                        Noob-club.ru - фансайт, посвященный многопользовательским онлайн играм World of Warcraft, Diablo и Star Wars: The Old Republic.
-                        Сайт написан на PHP c использованием Zend Framework. Оптимизирован под большие нагрузки.
+                        Noob-club.ru is a fansite dedicated to MMO games like World of Warcraft, Diablo and Star Wars: The Old Republic.
+                        Site is written on PHP with Zend Framework and optimized for constant highload.
                         ',
             ),
         ),
@@ -81,10 +72,10 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'Best Quest "Battleship"',
                 'description' => '
-                        Движок для уникального и увлекательного квеста в городских условиях от <a href="http://best-quest.ru/">Best Quest</a> на основе всем знакомой с детства игры.
-                        Проект реализован на Ruby on Rails.
-                        Дизайн нарисовал <a href="http://formazon.com/">Фарид Рафиков</a>.
-                        Подробности <a href="http://blog.evercodelab.com/battleship/">в блоге</a>.
+                        Engine for unique and fascinating real-life city quest by <a href="http://best-quest.ru/">Best Quest</a> based on widely known game.
+                        Built on Ruby on Rails.<br />
+                        Designed by <a href="http://formazon.com/">Formazon</a>.
+                        For more information see <a href="http://blog.evercodelab.com/battleship/">detailed blog entry</a>.
                         ',
             ),
         ),
@@ -99,9 +90,9 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'Saraswati.Pro',
                 'description' => '
-                        Издательство Сарасвати (Saraswati) нашло свое второе рождение в Таиланде, городе Чиангмай 20 января 2010 года и выпускает книги не только в традиционном формате, но также в виде цифровых публикаций и интерактивных приложения для iOS и Android.
-                        Наш первый проект на Ruby on Rails.
-                        Дизайн нарисовал <a href="http://formazon.com/">Фарид Рафиков</a>.
+                        Digital publishing house "Saraswati" was reborn in Thailand, Chiang Mai on 20th of January, 2010 and constantly releases books not only in traditional paper format but also in digital versions and interactive applications for iOs and Adroid.
+                        Our first project on Ruby on Rails.<br />
+                        Designed by <a href="http://formazon.com/">Formazon</a>.
                         ',
             ),
         ),
@@ -115,8 +106,8 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'Mahamandala',
                 'description' => '
-                        Медиа хранилище с широким списком социальных функций.
-                        Проект был разработан на Zend Framework + Doctrine первых версий. Сейчас портируется на Ruby on Rails.
+                        Media storage with wide list of social functionality.
+                        Originally was developed on first versions Zend Framework and Doctrine and is being ported to Ruby on Rails now.
                         ',
             ),
         ),
@@ -131,9 +122,9 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'DropNotify',
                 'description' => '
-                        Мини-сервис, составляющий красивый дайджест из изменений в ваших файлах и папках Dropbox за день и присылающий его на email.
-                        Разработан за пару дней для внутренних нужд. Работает на Google App Engine (Python).
-                        Подробности <a href="http://blog.evercodelab.com/dropnotify/">в блоге</a>.
+                        Mini-service which takes the feed of changes in your Dropbox files and folders, composes beutiful digest and sends it to your email everyday.
+                        Was developed in couple of days for inner needs. Works on Google App Engine (Python).
+                        For more information see <a href="http://blog.evercodelab.com/dropnotify/">detailed blog entry</a>.
                         ',
             ),
         ),
@@ -150,10 +141,10 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'The Book of Knowledge',
                 'description' => '
-                        Наша постоянно пополняемая база знаний.
-                        Спектр тем ограничен рамками нашего любопытства.
-                        Находится в открытом доступе.
-                        Работает на <a href="https://github.com/mojombo/jekyll">Jekyll</a> и <a href="http://pages.github.com/">github:pages</a>.
+                        Our constantly updated knowledge base.
+                        Variety of themes described is limited only by our curiosity.
+                        Available for everyone to see, share, comment and even contribute.
+                        Works on <a href="https://github.com/mojombo/jekyll">Jekyll</a> and <a href="http://pages.github.com/">github:pages</a>.
                         "When in doubt, consult the Book of Knowledge"
                         ',
             ),
@@ -170,10 +161,10 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'PHP Study Guide',
                 'description' => '
-                        Изначально — краткое руководство для подготовки к <a href="http://www.zend.com/en/services/certification/php-5-certification/">сертификации ZCE PHP5.3</a>.
-                        Немного переработано в более общее руководство для изучающих PHP.
-                        Находится в открытом доступе.
-                        Работает на <a href="https://github.com/mojombo/jekyll">Jekyll</a> и <a href="http://pages.github.com/">github:pages</a>.
+                        Originally was a small project that provides step by step guide on information needed to pass <a href="http://www.zend.com/en/services/certification/php-5-certification/">PHP5.3 ZCE certification</a>.
+                        For now it is a good place to start your experience with PHP5.3.
+                        Available for everyone to see, share, comment and even contribute.
+                        Works on <a href="https://github.com/mojombo/jekyll">Jekyll</a> and <a href="http://pages.github.com/">github:pages</a>.
                         ',
             ),
         ),
@@ -189,10 +180,10 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'EvercodeFaqBundle',
                 'description' => '
-                        Простой бандл для фреймворка Symfony2 для быстрой интеграции FAQ в проекты.
-                        Находится в открытом доступе.
-                        Вопросов не задает, но потенциально на них отвечает.
-                        Снижает нагрузку на support в разы.
+                        Simple Symfony2 Bundle for easy integrating FAQ functionality.
+                        Available for everyone to see, share, comment and even contribute.
+                        Doesn\'t ask question but potentially answers on them.
+                        Reduces working load on your support team.
                         ',
             ),
         ),
@@ -208,10 +199,10 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'EvercodeBannerBundle',
                 'description' => '
-                        Бандл для фреймворка Symfony2 для работы с баннерами.
-                        Находится в открытом доступе.
-                        Некоторые говорят, повышает конверсию на проектах, где используется.
-                        Умеет не только показывать баннеры, но и считать (яичницу не готовит).
+                        Symfony2 bundle for working with banner on your site.
+                        Available for everyone to see, share, comment and even contribute.
+                        Some say it increases conversion of your advertisement.
+                        Not only shows banners but also can count... from one to ten.
                         ',
             ),
         ),
@@ -227,10 +218,10 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'EvercodePageBundle',
                 'description' => '
-                        Очень простой экспериментальный бандл для фреймворка Symfony2, добавляющий в проекты функционал работы с информационными страницами.
-                        Находится в открытом доступе.
-                        Повышает уровень образованности посетителей сайтов на 146%.
-                        Улучшает навыки письменной речи.
+                        Dead simple experimental Symfony2 Bundle for easy management of your projects\' information pages.
+                        Available for everyone to see, share, comment and even contribute.
+                        Increases your visitors intellectual faculties by 146%.
+                        Significantly improves writing skills.
                         ',
             ),
         ),
@@ -245,9 +236,9 @@ class LoadProjectData extends AbstractFixture implements OrderedFixtureInterface
             'en' => array(
                 'name' => 'The EUSP Press’ internet store',
                 'description' => '
-                        В интернет-магазине Издательства Европейского Университета можно найти редкие, уникальные и интересные книги.
-                        По просьбе ЕУСПб мы уже не первый год занимаемся поддержкой и развитием сайта.
-                        Сайт работает на Zend Framework 1.
+                        In the EUSP Press’ internet store you can find interesting, rare and unique books.
+                        We are supporting the site for more than a year already.
+                        Site works on PHP with Zend Framework 1.
                         ',
             ),
         ),
