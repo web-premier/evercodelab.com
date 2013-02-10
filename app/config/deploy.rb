ee29c80fb543f66304db58bca87b41c9b007a594
Roma Lapin <roman.lapin@gmail.com>
2013-02-10 15:11:18 +0400

    updated deploy config

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -26,7 +26,8 @@ set :use_composer, true
 set :update_vendors, false
 set :symfony_env_prod, "prod"
 
-set  :keep_releases,  3
+set  :keep_releases,  2
+after "deploy", "deploy:cleanup"
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
@@ -34,11 +35,12 @@ logger.level = Logger::MAX_LEVEL
 set :writable_dirs,     ["app/cache", "app/logs"]
 set :webserver_user,    "www-data"
 set :permission_method, :acl
+set :use_set_permissions, true
 
 require 'hipchat/capistrano'
 
 set :hipchat_token, "915640c1f2d58ebf5d79d8ce4c1cd6"
 set :hipchat_room_name, "Github"
 set :hipchat_announce, false # notify users
-set :hipchat_color, 'green' #finished deployment message color
+set :hipchat_color, 'purple' #finished deployment message color
 set :hipchat_failed_color, 'red' #cancelled deployment message color
