34ca07c32d880cfc47687ea22b056b2458c0b46d
Sergey Lunev <sergey@lunyov.ru>
2013-12-19 15:50:16 +0400

    Slider in first view.

--- web/_new_html/js/main.js
+++ web/_new_html/js/main.js
@@ -1,1 +1,30 @@
+// Initalize slider
+var sliderInit = function() {
+    $("#slides").slidesjs({
+        height: 400,
+        navigation: {
+            effect: "fade"
+        },
+        pagination: {
+            effect: "fade"
+        },
+        play: {
+            active: false,
+            effect: "fade",
+            interval: 5000,
+            swap: false,
+            auto: true,
+            pauseOnHover: false,
+            restartDelay: 2500
+        },
+        effect: {
+            fade: {
+                speed: 800
+            }
+        }
+    });
+};
 
+$(document).ready(function(){
+    sliderInit();
+});
