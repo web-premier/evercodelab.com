2a1a0dc30183cb412ae9044ed8591c1670a8cb5c
Roma Lapin <roman.lapin@gmail.com>
2014-04-09 11:25:38 +0400

    umask

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,6 +4,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\Debug\Debug;
 
+umask(0000);
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
